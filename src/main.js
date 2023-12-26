d1ff3b66c8e0eccc20823dc418d99a890851fb22
Brian Ginsburg <gins@brianginsburg.com>
2023-12-26 11:23:17 -0800

    Add note emitter

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
+import Emittery from "emittery";
+
 import "./style.css";
 import * as ui from "./ui";
 import { Midi } from "./midi";
 
-const midi = new Midi(null);
+const noteEmitter = new Emittery();
+const midi = new Midi(noteEmitter);
 
 function getStarted() {
   ui.getStarted();
   midi.initialize(displayControllers);
 }
+
+noteEmitter.on("play", ({ midiNote }) => {
+  console.log("note on: ", midiNote);
+});
+noteEmitter.on("stop", ({ midiNote }) => {
+  console.log("note off: ", midiNote);
+});
 
 function setController(controller) {
   console.log("Setting controller: ", controller);
